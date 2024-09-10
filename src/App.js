92b2142a2ab781e60b21979a3f4ca2591bdb075e
Ankur Vedpathak <ankur.vedpathak@oracle.com>
2024-09-11 01:07:13 +0530

    Added text message configurations in App.js so that it becomes easy to change the text

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import React, { useState } from 'react'
 
 function App() {
 
+  //------------------------------------------------------------------
   // Dark Mode color configurations
   // -----------------------------------------------------------------
-  // Specify the background and text color for dark mode and light mode
+  const darkModeBackgroundColor = '#012154';
+  const darkModeTextColor = '#ffffff';
+  const lightModeBackgroundColor = '#ffffff';
+  const lighModeTextColor = '#000000';
+  const darkModeButtonText = 'Disable dark mode';
+  const lightModeButtonText = 'Enable dark mode';
+  //------------------------------------------------------------------
+  // Text message configurations
   // -----------------------------------------------------------------
-    const darkModeBackgroundColor = '#012154';
-    const darkModeTextColor = '#ffffff';
-    const lightModeBackgroundColor = '#ffffff';
-    const lighModeTextColor = '#000000';
-    const darkModeButtonText = 'Disable dark mode';
-    const lightModeButtonText = 'Enable dark mode';
+  const title = 'To Do list';
+  const taskCreated = 'Great, a new task has been created for you.';
+  const taskWarning = 'Uh Oh! The task you are creating is empty. Please try to enter some text :)';
+  const taskCompleted = 'Yayyy, your task has been completed!';
+  const noNewTasks = 'No tasks available. Add some tasks to get started!';
+  const noCompletedTasks = 'No tasks have been completed yet. Start by adding new tasks and mark them as you complete them.';
+  const success = 'success';
+  const warning = 'warning';
   //------------------------------------------------------------------
 
   // Initial state for dark/light mode
@@ -22,6 +32,7 @@ function App() {
     backgroundColor: lightModeBackgroundColor
   });
 
+  // Toggle dark/light mode button text
   const [darkModeText, setDarkModeText] = useState(lightModeButtonText);
 
   // To toggle darkmode
@@ -44,20 +55,20 @@ function App() {
 
   return (
     <div className="App" style={darkMode}>
-      <h1>To Do list</h1>
-      <div class="form-check form-switch" style={{position: 'relative', textAlign: 'left'}}>
-        <input className="form-check-input" onClick={toggleDarkMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+      <h1>{title}</h1>
+      <div class="form-check form-switch" style={{ position: 'relative', textAlign: 'left' }}>
+        <input className="form-check-input" onClick={toggleDarkMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
         <label>{darkModeText}</label>
       </div>
 
       <TextBox
-        taskCreated={"Great, a new task has been created for you."}
-        taskWarning={"Uh Oh! The task you are creating is empty. Please try to enter some text :)"}
-        taskCompleted={"Yayyy, your task has been completed!"}
-        success={"success"}
-        warning={"warning"}
-        noNewTasks={"No tasks available. Add some tasks to get started!"}
-        noCompletedTasks={"No tasks have been completed yet. Start by adding new tasks and mark them as you complete them."}
+        taskCreated={taskCreated}
+        taskWarning={taskWarning}
+        taskCompleted={taskCompleted}
+        success={success}
+        warning={warning}
+        noNewTasks={noNewTasks}
+        noCompletedTasks={noCompletedTasks}
         style={darkMode}
       />
     </div>
